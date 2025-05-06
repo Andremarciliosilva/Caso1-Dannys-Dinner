c4b8dcb8f680dd0981e860eea932515c4494a75b
Andre Silva <andremarciliosilva@gmail.com>
2025-05-06 16:05:14 -0300

    Resolução do exercício 4

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@ GROUP BY s.customer_id;
 -- 2. Há quantos dias cada cliente visita o restaurante?
 
 SELECT c.customer_id, 
-	   MAX(c.order_date) - MIN(c.order_date) AS dias
+	MAX(c.order_date) - MIN(c.order_date) AS dias
 FROM dannys_diner.sales c
 GROUP BY c.customer_id;
 
@@ -23,3 +23,18 @@ SELECT DISTINCT ON (customer_id)
   product_id AS produto
 FROM dannys_diner.sales
 ORDER BY customer_id, order_date;
+
+-- 4 Qual é o item mais comprado do menu e quantas vezes ele foi comprado por todos os clientes?
+
+SELECT 	
+  m.product_name,
+	s.product_id, 
+	COUNT(*) AS total_vezes_compra
+FROM dannys_diner.sales AS s
+INNER JOIN dannys_diner.menu AS m 
+	ON s.product_id = m.product_id
+GROUP BY s.product_id, m.product_name
+ORDER BY total_vezes_compra DESC
+LIMIT 1;
+
+
